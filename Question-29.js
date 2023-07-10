6ee2547f24465a90dede71f0e3dc2dca65120bf1
PIAIC-220943 <doctormalix@gmail.com>
2023-07-10 23:53:09 +0500

    QUESTION-29 Done

---
+++ Question-29.js
@@ -0,0 +1,12 @@
+// <<<<< QUESTION-29 >>>>>>>
+var favorite_fruits = ['banana', 'mango', 'grapes'];
+if (favorite_fruits.indexOf('apple') >= 0)
+    console.log('Your really like apples');
+if (favorite_fruits.indexOf('banana') >= 0)
+    console.log('Your really like Bananas');
+if (favorite_fruits.indexOf('orange') >= 0)
+    console.log('Your really like Oranges');
+if (favorite_fruits.indexOf('grapes') >= 0)
+    console.log('Your really like Grapes');
+if (favorite_fruits.indexOf('mango') >= 0)
+    console.log('Your really like Mangos');
