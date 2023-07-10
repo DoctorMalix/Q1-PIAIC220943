7e400c0d6f2c01ad2f1f6109e85cc046649f1d74
PIAIC-220943 <doctormalix@gmail.com>
2023-07-10 23:00:43 +0500

    QUESTION-26 Done

---
+++ Question-26.js
@@ -0,0 +1,15 @@
+// <<<<< QUESTION-26 >>>>>>>
+{
+    var alien_color = "Green";
+    if (alien_color === "Green")
+        console.log("\nPlayer just earned 5 points\n");
+    else
+        console.log("\nPlayer just earned 10 points\n");
+}
+{
+    var alien_color = "Red";
+    if (alien_color === "Green")
+        console.log("\nPlayer just earned 5 points\n");
+    else
+        console.log("\nPlayer just earned 10 points\n");
+}
---
+++ Question-26.ts
@@ -0,0 +1,17 @@
+// <<<<< QUESTION-26 >>>>>>>
+
+{
+    let alien_color: string = "Green"
+    if(alien_color === "Green")
+       console.log("\nPlayer just earned 5 points\n")
+    else 
+       console.log("\nPlayer just earned 10 points\n")
+}
+
+{
+    let alien_color: string = "Red"
+    if(alien_color === "Green")
+       console.log("\nPlayer just earned 5 points\n")
+    else 
+       console.log("\nPlayer just earned 10 points\n")
+}
