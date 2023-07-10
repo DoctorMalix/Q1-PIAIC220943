4f1442952998baf5ba624016ffc8761350eb7d91
PIAIC-220943 <doctormalix@gmail.com>
2023-07-10 23:24:51 +0500

    QUESTION-28 Done

---
+++ Question-28.ts
@@ -0,0 +1,15 @@
+// <<<<< QUESTION-28 >>>>>>>
+
+let age: number = 25
+if(age < 2)
+    console.log("The Persone is Baby")
+else if ((age >= 2) && (age< 4)) 
+    console.log("The Persone is Toddler")   
+else if ((age >= 4) && (age< 13)) 
+    console.log("The Persone is Kid")   
+else if ((age >= 13) && (age< 20)) 
+    console.log("The Persone is Teenager")
+else if ((age >= 20) && (age< 65)) 
+    console.log("The Persone is Adult") 
+else if (age >= 65) 
+    console.log("The Persone is Elder")      
