2237e9e09cb2cbeb76c524a4434a004a8c651781
PIAIC-220943 <doctormalix@gmail.com>
2023-07-10 23:07:21 +0500

    QUESTION-27 Done

---
+++ Question-27.js
@@ -0,0 +1,28 @@
+// <<<<< QUESTION-27 >>>>>>>
+{
+    var alien_color = "Green";
+    if (alien_color === "Green")
+        console.log("\nPlayer just earned 5 points\n");
+    else if (alien_color === "Yellow")
+        console.log("\nPlayer just earned 10 points\n");
+    else if (alien_color === "Red")
+        console.log("\nPlayer just earned 15 points\n");
+}
+{
+    var alien_color = "Yellow";
+    if (alien_color === "Green")
+        console.log("\nPlayer just earned 5 points\n");
+    else if (alien_color === "Yellow")
+        console.log("\nPlayer just earned 10 points\n");
+    else if (alien_color === "Red")
+        console.log("\nPlayer just earned 15 points\n");
+}
+{
+    var alien_color = "Red";
+    if (alien_color === "Green")
+        console.log("\nPlayer just earned 5 points\n");
+    else if (alien_color === "Yellow")
+        console.log("\nPlayer just earned 10 points\n");
+    else if (alien_color === "Red")
+        console.log("\nPlayer just earned 15 points\n");
+}
---
+++ Question-27.ts
@@ -0,0 +1,31 @@
+// <<<<< QUESTION-27 >>>>>>>
+
+{
+    let alien_color: string = "Green"
+    if(alien_color === "Green")
+       console.log("\nPlayer just earned 5 points\n")
+    else if(alien_color === "Yellow")
+       console.log("\nPlayer just earned 10 points\n")
+    else if(alien_color === "Red")
+    console.log("\nPlayer just earned 15 points\n")
+}
+
+{
+    let alien_color: string = "Yellow"
+    if(alien_color === "Green")
+    console.log("\nPlayer just earned 5 points\n")
+ else if(alien_color === "Yellow")
+    console.log("\nPlayer just earned 10 points\n")
+ else if(alien_color === "Red")
+ console.log("\nPlayer just earned 15 points\n")
+}
+
+{
+    let alien_color: string = "Red"
+    if(alien_color === "Green")
+    console.log("\nPlayer just earned 5 points\n")
+ else if(alien_color === "Yellow")
+    console.log("\nPlayer just earned 10 points\n")
+ else if(alien_color === "Red")
+ console.log("\nPlayer just earned 15 points\n")
+}
