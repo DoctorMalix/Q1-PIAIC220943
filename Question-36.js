e29995f89f04cb2156e33fe82db3755dad74d501
PIAIC-220943 <doctormalix@gmail.com>
2023-07-11 19:44:33 +0500

    QUESTION-36 Done

---
+++ Question-36.js
@@ -0,0 +1,9 @@
+// <<<<< QUESTION-36 >>>>>>>
+{ // this block is placed due to variables in this program warns that
+    // these variables are already declared in previous programs
+    function make_shirt(size, msg) {
+        console.log("The size of text is-> ", size);
+        console.log("The message is------>", msg);
+    }
+    make_shirt("Large", "Pakistan is a Greate country...");
+}
---
+++ Question-36.ts
@@ -0,0 +1,15 @@
+// <<<<< QUESTION-36 >>>>>>>
+
+{ // this block is placed due to variables in this program warns that
+  // these variables are already declared in previous programs
+
+  function make_shirt(size: string, msg: string )
+  {
+    console.log("The size of the Shirt-> ",size)
+    console.log("The message is------>",msg)
+  }
+
+  make_shirt("Large","Pakistan is a Greate country...")
+
+
+}
