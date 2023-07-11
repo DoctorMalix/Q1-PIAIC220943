5628b1c37e117fafb19bf816a0e884a95284898d
PIAIC-220943 <doctormalix@gmail.com>
2023-07-11 20:24:19 +0500

    QUESTION-39 Done

---
+++ Question-39.ts
@@ -0,0 +1,17 @@
+// <<<<< QUESTION-39 >>>>>>>
+
+{ // this block is placed due to variables in this program warns that
+    // these variables are already declared in previous programs
+  
+function describe_city(city: string, country:string = "Pakistan") 
+{
+    console.log("\"",city, ",", country,"\"")
+
+} 
+
+describe_city("Lahore")
+describe_city("Karachi")
+describe_city("Sydney","Australia")
+describe_city("Berlin","Germany")
+
+}
