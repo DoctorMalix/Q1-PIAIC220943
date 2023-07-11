bda566e022ce9a482cdfa55ebc7be3a1dc9bb2c2
PIAIC-220943 <doctormalix@gmail.com>
2023-07-11 19:49:58 +0500

    QUESTION-37 Done

---
+++ Question-37.ts
@@ -0,0 +1,19 @@
+// <<<<< QUESTION-37 >>>>>>>
+
+{ // this block is placed due to variables in this program warns that
+  // these variables are already declared in previous programs
+
+  function make_shirt(size: string ="Large", msg: string = "I love TypeScript" )
+  {
+    console.log("The size of the Shirt-> ",size)
+    console.log("The message is------>",msg)
+  }
+
+  make_shirt("Medium","Pakistan is a Greate country...")
+  make_shirt()
+  make_shirt("Small", "I Love Small Shirts")
+  make_shirt("Extra Large", "I Love Extra Large Shirts")
+
+
+
+}
