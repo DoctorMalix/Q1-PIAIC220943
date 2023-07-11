cc2686c718f048554bca68cb68fa5f72670342be
PIAIC-220943 <doctormalix@gmail.com>
2023-07-11 18:10:12 +0500

    QUESTION-34 Done

---
+++ Question-34.js
@@ -0,0 +1,12 @@
+// <<<<< QUESTION-34 >>>>>>>
+{ // this block is placed due to variables in this program warns that
+    // these variables are already declared in previous programs
+    var pizza = ['Fujjita', 'Chicken Tikka', 'Bar B.Q.'];
+    var i = 0;
+    console.log(pizza);
+    for (i = 0; i < pizza.length; i++) {
+        console.log("I like", pizza[i], "Pizza");
+    }
+    console.log("I love pizzas too much \n because pizzas are very delcious \n I eat pizzas every 2nd day.");
+    console.log("I really love pizza!");
+}
---
+++ Question-34.ts
@@ -0,0 +1,20 @@
+// <<<<< QUESTION-34 >>>>>>>
+
+{ // this block is placed due to variables in this program warns that
+  // these variables are already declared in previous programs
+
+let pizza: string[] = ['Fujjita', 'Chicken Tikka', 'Bar B.Q.'];
+
+let i:number = 0
+
+console.log(pizza)
+
+for (i =0; i < pizza.length; i++ )
+{
+  console.log("I like", pizza[i], "Pizza");
+}
+
+console.log("I love pizzas too much \n because pizzas are very delcious \n I eat pizzas every 2nd day.")
+
+console.log("I really love pizza!");
+}
