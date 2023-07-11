ac4b84664e46fcfe79bf254b6cbe0c8505bf6c01
PIAIC-220943 <doctormalix@gmail.com>
2023-07-11 22:12:54 +0500

    QUESTION-41 Done

---
+++ Question-41.js
@@ -0,0 +1,8 @@
+// <<<<< QUESTION-41 >>>>>>>
+{
+    var MagicArray = ["Kaboos", "Taboos", "Naboos", "Shaboos"];
+    function show_magicians(PrintMagicians) {
+        console.log(PrintMagicians);
+    }
+    show_magicians(MagicArray);
+}
---
+++ Question-41.ts
@@ -0,0 +1,14 @@
+// <<<<< QUESTION-41 >>>>>>>
+{
+
+let MagicArray: string[] = ["Kaboos","Taboos","Naboos","Shaboos"]
+
+    function show_magicians(PrintMagicians: string[])
+   {
+            console.log(PrintMagicians)
+
+    }
+
+    show_magicians(MagicArray)
+
+}
